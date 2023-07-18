0cd32b32564bfd542980440cf6b054d8ec88c72e
Etizaz Shah <139033592+etizazshah@users.noreply.github.com>
2023-07-18 00:17:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.write(
 st.info(
     """
     Privacy Note:
-    This bot collects and analyzes user feedback to improve our services. We respect your privacy and ensure that your data is used solely for research purposes. We do not share your personal information with any third parties. If you have any concerns, please contact us at random@email.com.
+    This bot collects and analyzes user feedback to improve our services. We respect your privacy and ensure that your data is used solely for research purposes. We do not share your personal information with any third parties. If you have any concerns, please contact us at support@email.com.
     """
 )
 
