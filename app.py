a33dc7438675a4210342ecf2a415772701abed9c
Etizaz Shah <139033592+etizazshah@users.noreply.github.com>
2023-07-19 03:04:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.sidebar.write(
     **OphyCare's Mission:**
     OphyCare is a post-revenue early stage company building Cloud Based Electronic Medical Record (EMR) for International Medical Relief Organizations like "Doctors without Borders". 90% of medical mission and relief organizations don't have proper software to manage patient data. More than 50% of these organizations use pen and paper and the ones who digitally store the data, use MS word or Google Drive. We are providing EPIC like EMR to these organizations where they can store the patient data which is accessible to doctors anywhere from the world. Along with storing patient data, it also gives live updates and statistical data to donors to track their money to the exact patient it was spent on. We have a huge traction and have launched our product with Palestine Children's Relief Fund with great success. We are looking for strategic partners, investors and advisors to join us.
     """
-
+)
 
 st.title("Sentiment Analysis Bot")
 
