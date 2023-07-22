ed71270613a5b247ea3f9cb8e3f55e5daf4f2c49
Etizaz Shah <139033592+etizazshah@users.noreply.github.com>
2023-07-22 14:37:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -98,10 +98,15 @@ if prompt := st.chat_input("Enter your feedback here"):
         # Check if the preprocessing resulted in an empty string
         if not new_text_preprocessed:
             raise ValueError("Could you please provide more context or rephrase your feedback?")
+         
+        # Check if the word "like" is in the preprocessed text and set sentiment accordingly
+        if "like" in new_text_preprocessed.lower():
+            sentiment = [1]  # Positive sentiment for "like"
+        else:
+            new_text_vectorized = text_vectorizer.transform([new_text_preprocessed]).toarray()
+            sentiment = naive_model.predict(new_text_vectorized)  # Replace `predict()` with the appropriate method for sentiment analysis
 
-        new_text_vectorized = text_vectorizer.transform([new_text_preprocessed]).toarray()
-        sentiment = naive_model.predict(new_text_vectorized)  # Replace `predict()` with the appropriate method for sentiment analysis
-
+    
         # Determine sentiment label
         if sentiment[0] == 1:
             response = "Seems like you are happy!"
